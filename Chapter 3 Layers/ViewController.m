561d9cf84896ca0f200d4e3fe07230ce6de77e21
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-05-06 16:21:37 +1200

    initial commit

---
+++ Chapter 3 Layers/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Chapter 3 Layers
+//
+//  Created by Daniel Wallace [DATACOM] on 6/05/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Chapter 3 Layers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chapter 3 Layers
+//
+//  Created by Daniel Wallace [DATACOM] on 6/05/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
