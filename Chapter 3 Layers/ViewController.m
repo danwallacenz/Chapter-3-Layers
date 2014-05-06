b9bc43470ac5caf5956a532b5b40cbd0e5dfbce4
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-05-06 16:38:08 +1200

    add three layers

--- Chapter 3 Layers/ViewController.m
+++ Chapter 3 Layers/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *drawingView;
 
 @end
 
@@ -18,6 +19,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CALayer *lay1 = [CALayer new];
+    lay1.frame = CGRectMake(113, 111, 132, 194);
+    lay1.backgroundColor = [[UIColor colorWithRed: 1 green: .4 blue: 1 alpha: 1] CGColor];
+    [self.drawingView.layer addSublayer:lay1];
+    
+    CALayer *lay2 = [CALayer new];
+    lay2.frame = CGRectMake(141, 156, 132, 194);
+    lay2.backgroundColor = [[UIColor colorWithRed: .5 green: 1 blue: 0 alpha: 1] CGColor];
+    [self.drawingView.layer addSublayer:lay2];
+    
+    CALayer *lay3 = [CALayer new];
+    lay3.frame = CGRectMake(43, 197, 160, 230);
+    lay3.backgroundColor = [[UIColor colorWithRed: 1 green: 0 blue: 0 alpha: 1] CGColor];
+    [self.drawingView.layer addSublayer:lay3];
+    
 }
 
 - (void)didReceiveMemoryWarning
